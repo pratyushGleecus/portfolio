76ad3dc91d3e5d83cf754d21e6f38a3b7c04b013
S-Eemani <133655996+S-Eemani@users.noreply.github.com>
2023-07-06 16:05:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,113 +1,26 @@
-function sendMail() {
 
-    let params = {
+function sendMail() {
+    var params = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
-        message: document.getElementById("message").value
-
-    }
-
-    var serviceID = process.env.EMAIL_SERVICE_ID;
-    var templateID = process.env.EMAIL_TEMPLATE_ID;
-
-
-    emailjs.send(serviceID, templateID, params)
-        .then(function (response) {
-            console.log('SUCCESS!', response.status, response.text);
-            alert("Success")
-        }, function (error) {
-            console.log('FAILED...', error);
-        });
-
-}
-
-function darkMode() {
-    let bodyEle = document.body;
-    bodyEle.classList.toggle("darkMode")
-    let square = document.getElementById("squareIcon")
-    square.classList.toggle("darkModeSq")
-}
-
-//Image Corousel
-
-var container = document.getElementById('container')
-var slider = document.getElementById('slider');
-var slides = document.getElementsByClassName('slide').length;
-var buttons = document.getElementsByClassName('btn');
-
-
-var currentPosition = 0;
-var currentMargin = 0;
-var slidesPerPage = 0;
-var slidesCount = slides - slidesPerPage;
-var containerWidth = container.offsetWidth;
-var prevKeyActive = false;
-var nextKeyActive = true;
-
-window.addEventListener("resize", checkWidth);
-
-function checkWidth() {
-    containerWidth = container.offsetWidth;
-    setParams(containerWidth);
-}
-
-function setParams(w) {
-    if (w < 551) {
-        slidesPerPage = 1;
-    } else {
-        if (w < 901) {
-            slidesPerPage = 2;
-        } else {
-            if (w < 1101) {
-                slidesPerPage = 3;
-            } else {
-                slidesPerPage = 4;
-            }
-        }
-    }
-    slidesCount = slides - slidesPerPage;
-    if (currentPosition > slidesCount) {
-        currentPosition -= slidesPerPage;
+        message: document.getElementById("message").value,
     };
-    currentMargin = - currentPosition * (100 / slidesPerPage);
-    slider.style.marginLeft = currentMargin + '%';
-    if (currentPosition > 0) {
-        buttons[0].classList.remove('inactive');
-    }
-    if (currentPosition < slidesCount) {
-        buttons[1].classList.remove('inactive');
-    }
-    if (currentPosition >= slidesCount) {
-        buttons[1].classList.add('inactive');
-    }
+    parameter = params
+    console.log("here")
+
+    
+
+    const serviceID = process.env.EMAIL_SERVICE_ID;
+    const templateID = process.env.EMAIL_TEMPLATE_ID;
+
+    emailjs.send(serviceID, templateID, params).then(res => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res)
+        alert("Success!!")
+    }).catch(err => {
+        console.log(err)
+    })
+
 }
-
-setParams();
-
-function slideRight() {
-    if (currentPosition != 0) {
-        slider.style.marginLeft = currentMargin + (100 / slidesPerPage) + '%';
-        currentMargin += (100 / slidesPerPage);
-        currentPosition--;
-    };
-    if (currentPosition === 0) {
-        buttons[0].classList.add('inactive');
-    }
-    if (currentPosition < slidesCount) {
-        buttons[1].classList.remove('inactive');
-    }
-};
-
-function slideLeft() {
-    if (currentPosition != slidesCount) {
-        slider.style.marginLeft = currentMargin - (100 / slidesPerPage) + '%';
-        currentMargin -= (100 / slidesPerPage);
-        currentPosition++;
-    };
-    if (currentPosition == slidesCount) {
-        buttons[1].classList.add('inactive');
-    }
-    if (currentPosition > 0) {
-        buttons[0].classList.remove('inactive');
-    }
-};
